5e3b4821e78fc1c6c2f97dc1a653f5ade61e368a
Dave Laird <kiwiupover@gmail.com>
2017-11-05 14:11:24 -0800

    Use any serializer type with fastboot

--- app/instance-initializers/ember-data-fastboot.js
+++ app/instance-initializers/ember-data-fastboot.js
@@ -1,10 +1,26 @@
+import { isEmpty } from '@ember/utils';
+import { debug } from '@ember/debug';
+
 export function initialize(applicationInstance) {
   let shoebox = applicationInstance.lookup('service:fastboot').get('shoebox');
   if (!shoebox) { return; }
   let dump = shoebox.retrieve('ember-data-store');
   if (!dump) { return; }
   let store = applicationInstance.lookup('service:store');
-  store.pushPayload(dump.records);
+
+  Object.keys(dump.records).forEach(modelName => {
+    let recordsToPush = {};
+    recordsToPush[modelName] = dump.records[modelName];
+    if (isEmpty(recordsToPush[modelName])) { return; }
+
+    if (Object.keys(recordsToPush[modelName]).includes('data')) {
+      store.pushPayload(modelName, recordsToPush[modelName]);
+      return;
+    }
+
+    let normalizeData = recordsToPush[modelName].map(o => store.normalize(modelName, o).data);
+    return store.push({ data: normalizeData });
+  });
 }
 
 export default {
--- fastboot/instance-initializers/ember-data-fastboot.js
+++ fastboot/instance-initializers/ember-data-fastboot.js
@@ -5,12 +5,28 @@ export function initialize(applicationInstance) {
 
   shoebox.put('ember-data-store', {
     get records() {
-      return modelNames.map(name => {
+      let serializedRecords = {};
+
+      modelNames.map(name => {
+        serializedRecords[name] = [];
         return store.peekAll(name).toArray();
       }).reduce((a,b) => a.concat(b), [])
         .filter(record => record.get('isLoaded') && !record.get('isNew'))
-        .map(record => record.serialize({ includeId: true}))
-        .reduce((a,b) => { a.data.push(b.data); return a; }, { data: [] });
+        .forEach(record => {
+          let modelName = record._internalModel.modelName;
+          let serialized = record.serialize({ includeId: true });
+
+          if (Object.keys(serialized).includes('data')) {
+            if (serializedRecords[modelName].length === 0) {
+              serializedRecords[modelName] = { data: [] };
+            }
+            serializedRecords[modelName]['data'].push(serialized.data);
+          } else {
+            serializedRecords[modelName].push(serialized);
+          }
+        });
+
+      return serializedRecords;
     }
   });
 }
